553adceeb5b26db01a817e507c33afd4785f2c92
DindaShaumiSalsabila <dindashaumi67@gmail.com>
2024-11-23 22:10:27 +0700

    Update loaading1.cpp

--- loaading1.cpp
+++ loaading1.cpp
@@ -1,9 +1,28 @@
-p#include <ncurses.h>
-#include <string>
-#include <thread>
-#include <chrono>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ncurses.h>
+#include <unistd.h>
 
-void animateLogin() {
-    const std::string loading = "Loading...";
-    int x = 0;
+#define MAX_USERNAME_LENGTH 20
+#define MAX_PASSWORD_LENGTH 20
 
+void loading_screen() {
+    clear();
+    printw("Loading");
+    for (int i = 0; i < 10; i++) {
+        printw(".");
+        refresh();
+        usleep(500000); // 0.5 detik
+    }
+    clear();
+}
+
+void get_user_credentials(char *username, char *password) {
+    echo();
+    printw("Masukkan Username: ");
+    getnstr(username, MAX_USERNAME_LENGTH);
+    printw("Masukkan Password: ");
+    getnstr(password, MAX_PASSWORD_LENGTH);
+    noecho();
+}
