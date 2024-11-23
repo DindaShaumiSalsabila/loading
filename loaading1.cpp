6db57269be7773638b3f5f43c282b5ccbb5e1580
nawuraa <nauraazura33@gmail.com>
2024-11-23 17:48:42 +0700

    loaading1.cpp

--- loaading1.cpp
+++ loaading1.cpp
@@ -1,4 +1,4 @@
-#include <ncurses.h>
+p#include <ncurses.h>
 #include <string>
 #include <thread>
 #include <chrono>
@@ -7,25 +7,3 @@ void animateLogin() {
     const std::string loading = "Loading...";
     int x = 0;
 
-while (true) {
-        clear();
-        mvprintw(10, x, loading.c_str());
-        refresh();
-        x++;
-        if (x > COLS) {
-            x = 0;
-        }
-std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-}
-
-int main() {
-    // Inisialisasi ncurses
-    initscr();
-    cbreak(); // Mengaktifkan mode cbreak
-    noecho(); // Menyembunyikan input pengguna
-    curs_set(0); // Menyembunyikan kursor
-
-    // Mengatur warna
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
