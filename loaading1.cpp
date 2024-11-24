ea7dd9566bb4bf80043e4afdb0e9356d1c4ea444
nawuraa <nauraazura33@gmail.com>
2024-11-24 12:48:32 +0700

    loaading1.cpp

--- loaading1.cpp
+++ loaading1.cpp
@@ -26,3 +26,23 @@ void get_user_credentials(char *username, char *password) {
     getnstr(password, MAX_PASSWORD_LENGTH);
     noecho();
 }
+
+void save_credentials(const char *username, const char *password) {
+    FILE *file = fopen("credentials.txt", "w");
+    if (file != NULL) {
+        fprintf(file, "Username: %s\n", username);
+        fprintf(file, "Password: %s\n", password);
+        fclose(file);
+    } else {
+        printw("Gagal menyimpan data.\n");
+        refresh();
+        sleep(2);
+    }
+}
+
+void welcome_message(const char *username) {
+    clear();
+    printw("Selamat datang, %s!\n", username);
+    refresh();
+    sleep(2);
+}
